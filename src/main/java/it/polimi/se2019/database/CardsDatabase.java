da1c1d3411e3fd522e2328023d1ee687aaf24307
matteo12f <matteofioretto@icloud.com>
2019-03-31 17:19:34 +0200

    Caricamento package database delle carte

---
+++ src/main/java/it/polimi/se2019/database/CardsDatabase.java
@@ -0,0 +1,19 @@
+package it.polimi.se2019.database;
+
+public class CardsDatabase {
+
+    //Default constructor
+    public CardsDatabase() {
+    }
+    private GunCard gunDatabase;
+    private PowerupCard powerupsDatabase;
+    private AmmoTileCard ammotilesDatabase;
+
+    public void setAmmotilesDatabase(AmmoTileCard ammotilesDatabase) {
+        this.ammotilesDatabase = ammotilesDatabase;
+    }
+
+    public void CardsDatabase() {
+    }
+
+}
---
+++ src/main/java/it/polimi/se2019/database/PowerupCard.java
@@ -0,0 +1,40 @@
+package it.polimi.se2019.database;
+
+public class PowerupCard {
+
+    /**
+     * Default constructor
+     */
+    public PowerupCard() {
+    }
+    private int id;
+    private String powerupType;
+    private char cubeColor;
+    public void PowerupCard() {
+    }
+
+    /**
+     * @return
+     */
+    public int getId() {
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public String getPowerupType() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public char getCubeColor() {
+        return '0';
+    }
+
+    public void doAction() {
+    }
+
+}
