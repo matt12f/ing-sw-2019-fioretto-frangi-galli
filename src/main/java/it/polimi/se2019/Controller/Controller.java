07e2e425715ade9e60119edd9721405ff959dc1b
StefanoGalliPoli <48619635+StefanoGalliPoli@users.noreply.github.com>
2019-04-25 12:33:10 +0200

    Update Controller.java

--- src/main/java/it/polimi/se2019/Controller/Controller.java
+++ src/main/java/it/polimi/se2019/Controller/Controller.java
@@ -1,1 +1,68 @@
 
+package it.polimi.se2019.controller;
+
+import it.polimi.se2019.model.game.GameModel;
+import it.polimi.se2019.model.game.Player;
+import it.polimi.se2019.view.RemoteView;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Controller implements Observer {
+    private GameModel mainGameModel;
+    private ArrayList<RemoteView> remoteViews;
+    private TurnManager activeturn;
+
+    /**
+     * This constructor generates one local Controller object for one Player
+     */
+    public Controller() {
+        ArrayList<Player> players = new ArrayList<>();
+        String nickname="Da fare";
+        char color='b';
+        //TODO chiedere nickname passando da VIEW
+        //TODO scelta del colore passando da VIEW
+        players.add(new Player(1,nickname,color));
+        //TODO chiedere la modalità di gioco
+        String gameMode="normal";
+        this.mainGameModel=new GameModel(players,gameMode);
+        //TODO capire come usare le remoteviews
+        this.remoteViews=new ArrayList<>();
+        this.remoteViews.add(new RemoteView());
+        this.activeturn = new TurnManager();
+    }
+
+    public GameModel getMainGameModel() {
+        return mainGameModel;
+    }
+
+    public ArrayList<RemoteView> getLocalView() {
+        return remoteViews;
+    }
+
+    public TurnManager getActiveturn() {
+        return activeturn;
+    }
+
+    public GameStats playGame() {
+        setupGame();
+        //TODO ciclo che fa partire startTurn()
+        return new GameStats(mainGameModel.getPlayerList(),mainGameModel.getTurn());
+    }
+
+    private void setupGame(){
+        //TODO riempire spawnCell con carte
+        //TODO riempire dropCell con carte
+
+    }
+
+    private void startTurn(){
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
