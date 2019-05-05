2ef513c1d2d2a6658957a3387293a87024cc6c9c
Frangi97 <10563288@polimi.it>
2019-05-05 12:16:36 +0200

    Aggiunto scheletro SOCKET

---
+++ src/main/java/it/polimi/se2019/network/socketClient.java
@@ -0,0 +1,22 @@
+package it.polimi.se2019.network;
+
+import java.net.*;
+import java.io.*;
+
+public class socketClient {
+    private Socket socket;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+
+    public socketClient(){
+
+    }
+
+    public void send(ObjectOutputStream obj){
+
+    }
+
+    public void receive(ObjectInputStream obj){
+
+    }
+}
---
+++ src/main/java/it/polimi/se2019/network/socketServer.java
@@ -0,0 +1,24 @@
+package it.polimi.se2019.network;
+
+import java.net.*;
+import java.io.*;
+
+public class socketServer {
+    private Socket socket;
+    private ServerSocket server;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+
+    public socketServer(){
+
+    }
+
+
+    public void send(ObjectOutputStream obj){
+
+    }
+
+    public void receive(ObjectInputStream obj){
+
+    }
+}
