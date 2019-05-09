1c1d4a00f3db941cce1ff6f4fbff219f708f2c68
Frangi97 <10563288@polimi.it>
2019-05-09 22:09:43 +0200

    Ffixed ActionTileView

--- src/main/java/it/polimi/se2019/view/ActionTileView.java
+++ src/main/java/it/polimi/se2019/view/ActionTileView.java
@@ -5,6 +5,18 @@ public class ActionTileView {
     private boolean adrenaline1;
     private boolean adrenaline2;
 
+    public void setAdrenaline1(boolean adrenaline1) {
+        this.adrenaline1 = adrenaline1;
+    }
+
+    public void setAdrenaline2(boolean adrenaline2) {
+        this.adrenaline2 = adrenaline2;
+    }
+
+    public void setFront(boolean front) {
+        this.front = front;
+    }
+
     public boolean isFront() {
         return front;
     }
