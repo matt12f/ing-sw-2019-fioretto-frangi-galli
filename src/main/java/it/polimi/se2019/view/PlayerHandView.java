29f958d32763daf70154eb719e723efaa1b6efab
Frangi97 <10563288@polimi.it>
2019-05-09 21:59:28 +0200

    Inizio implementazione di observer/observable

---
+++ src/main/java/it/polimi/se2019/view/PlayerHandView.java
@@ -0,0 +1,14 @@
+package it.polimi.se2019.view;
+
+
+public class PlayerHandView {
+    private CardTileView[] weapons;
+
+    public void setWeapons(CardTileView[] weapons) {
+        this.weapons = weapons;
+    }
+
+    public CardTileView[] getWeapons() {
+        return weapons;
+    }
+}
