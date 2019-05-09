856ab93328b2bceaa4d1fe37a649627ab846a661
Frangi97 <10563288@polimi.it>
2019-05-09 21:58:00 +0200

    Inizio implementazione di observer/observable

--- src/main/java/it/polimi/se2019/View/CardTileView.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.se2019.view;
-
-public class CardTileView {
-    private String image;
-    private String type;
-}
---
+++ src/main/java/it/polimi/se2019/view/CardTileView.java
@@ -0,0 +1,22 @@
+package it.polimi.se2019.view;
+
+public class CardTileView {
+    private String image;
+    private String type;
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
