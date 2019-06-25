5875e80098f837fff843e72826ea87f286498a57
StefanoGalliPoli <10531674@polimi.it>
2019-06-25 15:30:34 +0200

    gui update

--- src/main/java/it/polimi/se2019/view/MapViewGUI.java
+++ src/main/java/it/polimi/se2019/view/MapViewGUI.java
@@ -8,7 +8,17 @@ public class MapViewGUI extends JPanel {
 
     private ImageIcon map;
     protected JLabel labelBackground;
+    private WeaponButtonGUI weaponblue1;
+    private WeaponButtonGUI weaponblue2;
+    private WeaponButtonGUI weaponblue3;
 
+    private WeaponButtonGUI weaponred1;
+    private WeaponButtonGUI weaponred2;
+    private WeaponButtonGUI weaponred3;
+
+    private WeaponButtonGUI weaponyellow1;
+    private WeaponButtonGUI weaponyellow2;
+    private WeaponButtonGUI weaponyellow3;
     public MapViewGUI(int config){
 
 
@@ -26,14 +36,53 @@ public class MapViewGUI extends JPanel {
         this.labelBackground = new JLabel(map);
         labelBackground.setIcon(map);
 
+        ///////blue weapons
+         this.weaponblue1 = new WeaponButtonGUI(56,80);
+         this.weaponblue1.setLocation(317, 0);
 
-        JPanel mainPanel = new JPanel(new GridBagLayout());
+        this.weaponblue2 = new WeaponButtonGUI(56,80);
+        this.weaponblue2.setLocation(382, 0);
+
+        this.weaponblue3 = new WeaponButtonGUI(56,80);
+        this.weaponblue3.setLocation(448, 0);
+
+        ////////red weapons
+
+        this.weaponred1 = new WeaponButtonGUI(80,56);
+        this.weaponred1.setLocation(0, 160);
+
+        this.weaponred2 = new WeaponButtonGUI(80,56);
+        this.weaponred2.setLocation(0, 225);
+
+        this.weaponred3 = new WeaponButtonGUI(80,56);
+        this.weaponred3.setLocation(0, 286);
+
+        ///////yellow weapons
+
+        this.weaponyellow1 = new WeaponButtonGUI(80,56);
+        this.weaponyellow1.setLocation(518, 250);
+
+        this.weaponyellow2 = new WeaponButtonGUI(80,56);
+        this.weaponyellow2.setLocation(518, 311);
+
+        this.weaponyellow3 = new WeaponButtonGUI(80,56);
+        this.weaponyellow3.setLocation(518, 375);
+
+
+
         GridBagConstraints container = new GridBagConstraints();
-        mainPanel.setLayout(new GridBagLayout());
-
 
         setLayout(new GridBagLayout());
         //labelBackground.add(mainPanel);
+        labelBackground.add(weaponblue1);
+        labelBackground.add(weaponblue2);
+        labelBackground.add(weaponblue3);
+        labelBackground.add(weaponred1);
+        labelBackground.add(weaponred2);
+        labelBackground.add(weaponred3);
+        labelBackground.add(weaponyellow1);
+        labelBackground.add(weaponyellow2);
+        labelBackground.add(weaponyellow3);
         add(labelBackground,container);
 
         //container.gridx= 0;
--- src/main/java/it/polimi/se2019/view/WeaponButtonGUI.java
+++ src/main/java/it/polimi/se2019/view/WeaponButtonGUI.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class WeaponButtonGUI extends JButton {
 
     public WeaponButtonGUI(int x, int y){
+        setSize(x,y);
+
         ImageIcon wpImage1 = new ImageIcon(new ImageIcon("src/main/sprite/cards/weapons/weapons_back.png").getImage().getScaledInstance(x,y, Image.SCALE_DEFAULT));
         setIcon(wpImage1);
     }
