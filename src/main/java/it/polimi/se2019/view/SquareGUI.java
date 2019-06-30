2684fd3b9d9a71cebed1153e1690df1360665182
StefanoGalliPoli <10531674@polimi.it>
2019-06-30 18:01:48 +0200

    gui update

--- src/main/java/it/polimi/se2019/view/SquareGUI.java
+++ src/main/java/it/polimi/se2019/view/SquareGUI.java
@@ -1,12 +1,20 @@
 package it.polimi.se2019.view;
 
+import it.polimi.se2019.model.cards.AmmoTileCard;
+import it.polimi.se2019.model.game.Figure;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class SquareGUI extends JLabel {
+    public Square data;
+
 
     public SquareGUI(){
         //TODO appena capiamo come erediare al posto di type mettere figure.color se != null, altrimenti se drop !=null mettere nero
+
+        data = new Square();
+
         updateImage(null);
         setSize(20,20);
     }
@@ -18,6 +26,15 @@ public class SquareGUI extends JLabel {
             setIcon(pic);
         }else{
             setIcon(null);
+        }
+
+    }
+    public void setContent(Figure figure, AmmoTileCard drop){
+
+        if(figure == null){
+            data.setFigure(figure);
+        }else {
+            data.setDrop(drop);
         }
 
     }
--- src/main/java/it/polimi/se2019/view/WeaponButtonGUI.java
+++ src/main/java/it/polimi/se2019/view/WeaponButtonGUI.java
@@ -1,5 +1,7 @@
 package it.polimi.se2019.view;
 
+import it.polimi.se2019.model.cards.GunCard;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,5 +12,14 @@ public class WeaponButtonGUI extends JButton {
 
         ImageIcon wpImage1 = new ImageIcon(new ImageIcon("src/main/sprite/cards/weapons/weapons_back.png").getImage().getScaledInstance(x,y, Image.SCALE_DEFAULT));
         setIcon(wpImage1);
+    }
+
+    public void updateImage(GunCard weapon){
+        String weaponType = weapon.toString();
+
+        switch (weaponType){
+            case "":
+        }
+
     }
 }
