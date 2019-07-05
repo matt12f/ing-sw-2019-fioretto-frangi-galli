ff50b5d3c22689aeccdd78baf3bf600ed01db240
StefanoGalliPoli <10531674@polimi.it>
2019-07-05 14:31:56 +0200

    gui update

--- src/main/java/it/polimi/se2019/view/CellViewGUI.java
+++ src/main/java/it/polimi/se2019/view/CellViewGUI.java
@@ -42,7 +42,7 @@ public class CellViewGUI extends JPanel {
     }
 
     public void updateCell(CellView cell){
-
+        removeAll();
         for (int i = 0; i<3 ; i++){
             for (int j = 0; j< 3; j++){
                 if(j==0 && i==0 && cell.getDrop() != null){
