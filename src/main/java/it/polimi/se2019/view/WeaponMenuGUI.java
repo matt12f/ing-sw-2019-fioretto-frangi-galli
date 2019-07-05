969e8a44a964f7035621bbccf6a37e1a01e4f5da
StefanoGalliPoli <10531674@polimi.it>
2019-07-05 20:42:04 +0200

    gui update

--- src/main/java/it/polimi/se2019/view/WeaponMenuGUI.java
+++ src/main/java/it/polimi/se2019/view/WeaponMenuGUI.java
@@ -53,7 +53,7 @@ public class WeaponMenuGUI extends JFrame{
 
         addWindowListener(new WeaponMenuGUI.CloseListener());
         add(mainPanel);
-        setSize(700,700);
+        setSize(1000,700);
         setLocation(500,500);
         setVisible(true);
 
