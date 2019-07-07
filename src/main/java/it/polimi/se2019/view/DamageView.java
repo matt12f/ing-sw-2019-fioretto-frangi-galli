36b5760d5efdad020255c8b8d5ab8a71d5e2355c
StefanoGalliPoli <10531674@polimi.it>
2019-07-07 15:42:42 +0200

    gui update

--- src/main/java/it/polimi/se2019/view/DamageView.java
+++ src/main/java/it/polimi/se2019/view/DamageView.java
@@ -7,6 +7,10 @@ public class DamageView implements Serializable {
     private char[] damage;
     private ArrayList<Character> marks;
 
+    /**
+     * create the view data of the damage and marks of a player
+     * @param damageFromModel list of the player's damage
+     */
     public DamageView(char [] damageFromModel){
         this.damage = new char[12];
         for (int i = 0; i < 12; i++)
@@ -14,18 +18,34 @@ public class DamageView implements Serializable {
 
     }
 
+    /**
+     * update the list of the damage
+     * @param damage list of the damage to update
+     */
     public void setDamage(char[] damage) {
         this.damage = damage;
     }
 
+    /**
+     *
+     * @return the list of the marks
+     */
     public ArrayList<Character> getMarks() {
         return marks;
     }
 
+    /**
+     * update of the player's marks
+     * @param marks
+     */
     public void setMarks(ArrayList<Character> marks) {
         this.marks = marks;
     }
 
+    /**
+     *
+     * @return the damage list
+     */
     public char[] getDamage() {
         return damage;
     }
