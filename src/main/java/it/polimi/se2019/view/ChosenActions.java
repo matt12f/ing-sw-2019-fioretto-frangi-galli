4b4192a87cfce0561df520d4869b557f4560b06d
matteo12f <matteofioretto@icloud.com>
2019-07-12 11:12:32 +0200

    Fixed error that would occur in shockwave

--- src/main/java/it/polimi/se2019/view/ChosenActions.java
+++ src/main/java/it/polimi/se2019/view/ChosenActions.java
@@ -185,7 +185,13 @@ public class ChosenActions implements Serializable {
      * @return cell to move yourself/a Player in
      */
     private NewCell selectCellWithTargets(LocalView localView, SingleEffectsCombinationActions combination, String mode) {
-        int maxCell=combination.getMaxCellToSelect();
+        int maxCell;
+        //this evaluates and sets the maximum number of cells to select
+        if(combination.getCellsWithTargets().size() < combination.getMaxCellToSelect())
+            maxCell=combination.getCellsWithTargets().size();
+        else
+            maxCell=combination.getMaxCellToSelect();
+
         ArrayList<CellWithTargets> cellList=combination.getCellsWithTargets();
         CellWithTargets arrivalCell;
 
@@ -262,7 +268,7 @@ public class ChosenActions implements Serializable {
                 cont++;
                 //lists as strings all of the cells, for the player to then select one to choose targets on
                 stringList = listCellWithTargets(localView, cellList);
-                //TODO magari sposta while prima, da eccezione con shockwave
+
                 arrivalCell = possibleCells.get(stringList.indexOf(this.askUser.stringSelector("Scegli la cella in cui poi selezionare il " + cont + "° target da colpire", stringList)));
                 cellList.remove(arrivalCell); //removes cell already selected
                 this.targetsFromCell.addAll(targetSelectionFromCell(arrivalCell));
